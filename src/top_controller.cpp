e4b7a59a1625bebb06539f3adcc8bc2c7f02863d
Chenyu Zhang <zhangcy73k@gmail.com>
2018-04-03 15:57:44 +0800

    basically finished top controller

--- src/top_controller.cpp
+++ src/top_controller.cpp
@@ -11,55 +11,91 @@ TopController::TopController(Tap *t) : tap(t) {
     load = 0;
 }
 
-bool TopController::load_config() {
+void TopController::load_config() {
 
     std::string prefix = "/home/" + std::string(getlogin()) + "/";
+
     std::string default_latency_file = prefix + "latency_info";
-    latency_path = get_opt("HERACLES_LATENCY_FILE", default_latency_file);
-
+    std::string default_max_latency_file = prefix + "max_latency_info";
     std::string default_load_file = prefix + "load_info";
-    load_path = get_opt("HERACLES_LOAD_FILE", default_load_file);
-
     std::string default_max_load_file = prefix + "max_load_info";
+
+    latency_path = get_opt("HERACLES_LATENCY_FILE", default_latency_file);
+    max_latency_path =
+        get_opt("HERACLES_MAX_LATENCY_FILE", default_max_latency_file);
+    load_path = get_opt("HERACLES_LOAD_FILE", default_load_file);
     max_load_file = get_opt("HERACLES_MAX_LOAD_FILE", default_max_load_file);
+
+    sleep_time = get_opt("TOP_SLEEP_TIME", 5);
+    disable_bound = get_opt("TOP_DISABLE_BOUND", 0.85);
+    enable_bound = get_opt("TOP_ENABLE_BOUND", 0.8);
+    slow_be_bound = get_opt("TOP_SLOW_BE_BOUND", 0.1);
 }
+
 bool TopController::update() {
     ifstream load_in(load_path);
-    if(!load_in.is_open()) {
-        print_err("can't open load file.");
-        return false;
-    }
-    if(!load_in.eof()) {
-        print_err("nothing in load file.");
+    if (!load_in.is_open() || !load_in.eof()) {
+        print_err("can't read load file.");
         return false;
     }
     load_in >> cur_load;
     load_in.close();
 
     ifstream max_load_in(max_load_path);
-    if(!max_load_in.is_open()) {
-        print_err("can't open max_load file.");
-        return false;
-    }
-    if(!max_load_in.eof()) {
-        print_err("nothing in max_load file.");
+    if (!max_load_in.is_open() || !max_load_in.eof()) {
+        print_err("can't read max_load file.");
         return false;
     }
     max_load_in >> max_load;
     max_load_in.close();
 
     ifstream latency_in(latency_path);
-    if(!latency_in.is_open()) {
+    if (!latency_in.is_open() || !latency_in.eof()) {
         print_err("can't open latency file.");
-        return false;
-    }
-    if(!latency_in.eof()) {
-        print_err("nothing in latency file.");
         return false;
     }
     latency_in >> l95 >> l99 >> lmax;
     latency_in.close();
+
+    ifstream max_latency_in(max_latency_path);
+    if (!max_latency_in.is_open() || !max_latency_in.eof()) {
+        print_err("can't open max_latency file.");
+        return false;
+    }
+    max_latency_in >> max_latency;
+    max_latency_in.close();
+
     return true;
 }
 
-int run() {}
+int TopController::run() {
+
+    load_config();
+
+    struct timespec ts;
+    ts.tv_sec = sleep_time;
+    ts.tv_nsec = 0;
+
+    while (true) {
+        nanosleep(ts);
+        if (!update()) {
+            return -1;
+        }
+        double slack = (max_latency - latency) / max_latency;
+        double load_percent = load / max_load;
+        if (slack < 0) {
+            tap->disable_BE();
+            // enter_cooling_down()...
+        } else if (load_percent > disable_bound) {
+            tap->disable_BE();
+        } else if (load_percent < enable_bound) {
+            tap->enable_BE();
+        } else if (slack < slow_be_bound) {
+            // disallow_be_growth()...
+            // if (slack < slow_be_bound / 2) {
+            //     be_cores.remove(be_cores.size() - 2)
+            // }
+        }
+    }
+    return 0;
+}
--- src/top_controller.h
+++ src/top_controller.h
@@ -3,26 +3,35 @@
 
 #include "tap.h"
 #include <string>
+#include <ctime>
 
 class TopController {
   private:
     std::string latency_path;
+    std::string max_latency_path;
     std::string max_load_path;
     str::string load_path;
+
+    time_t sleep_time;
+    double disable_bound;
+    double enable_bound;
+    double slow_be_bound;
+    
     Tap *tap;
 
     double l95;
     double l99;
     double lmax;
+    double max_latency;
+
     int max_load;
     int load;
 
+    void load_config();
     bool update();
 
   public:
     TopController(Tap *t);
-
-    bool is_config_ok();
 
     int run();
 };
